1213d3ef0ba323045147ea5e19e21522f36246f2
Louis Thiery <me@louisthiery.com>
2012-05-05 11:44:46 -0400

    added plots generation

--- fidoSimulator.py
+++ fidoSimulator.py
@@ -1,14 +1,18 @@
 #!bin/env/python
 
 import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
 
 class Greenhouse:
 
     def __init__(self, initialTempOutside, initialSun,
                  diffusion=random.random()/4+0.25,
-                 radiation=random.random()/16+0.05, 
-                 fan=random.random()/4, 
-                 heater=random.random()/4):
+                 radiation=random.random()/16+0.25, 
+                 fan=random.random()/4+0.25, 
+                 heater=random.random()*10):
         self.tempI = initialTempOutside + radiation*initialSun
         self.dif=diffusion
         self.fan=fan
@@ -23,12 +27,33 @@ class Greenhouse:
         
         self.tempI = self.tempI*(1-flux) + tempOutside*flux + self.rad*sun
         
-        if(wantHeater): self.tempI+=heat
+        if(wantHeater): self.tempI+=self.heat
 
-        return (tempOutside,self.tempI)
+        return self.tempI
 
-house = Greenhouse(80,50)
-for i in range(0,32):
-    print house.step(80,50,False,False)
+tempI = 60
+sunI = 50
+house = Greenhouse(tempI,sunI)
+temps = [70]
+y1 = [tempI]
+suns = [50]
+endRange = 1024
 
-        
+for i in range(0,endRange):
+
+    temperature = 70+25*np.sin(i*math.pi/124)
+    sun = max(0,50*np.sin(i*math.pi/124))
+    fan = False; heater = False;
+    if y1[i]>90: fan=True
+    if y1[i]<65: heater=True
+    suns += [sun]
+    temps += [temperature]
+    y1 += [house.step(temperature,sun,fan,heater)]
+
+x = range(0,endRange+1)
+
+
+plt.plot(x,temps)
+plt.plot(x,suns)
+plt.plot(x,y1)
+pylab.show()
